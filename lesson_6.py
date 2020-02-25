e8a494567fb5a9b2788f311cb71166b1de331eea
Olllyalya <56741204+Olllyalya@users.noreply.github.com>
2020-02-25 13:16:30 +0300

    Update lesson_6.py

--- lesson_6.py
+++ lesson_6.py
@@ -11,3 +11,18 @@ class EvenIterator(object):
             print(i)
         else:
             raise StopIteration
+
+            
+            
+            
+ list_generat = [arg for arg in dir(Foo) if not arg.startswith('_')]
+
+
+
+
+from itertools import zip_longest
+
+list_1 = [1,2,3]
+list_2 = ['a','b','c','d']
+
+final_dist = {k: v for k, v in zip_longest(list_1, list_2) if k is not None}
